a0df5ca90d75a970bcd6f09a29ea339c1e1f4d5a
Artur Cunha <arturfernandescunha@hotmail.com>
2022-09-13 11:21:53 -0300

    dag update

--- dags/dag_example_results.py
+++ dags/dag_example_results.py
@@ -1,19 +1,41 @@
+
 import pandas as pd
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 #from airflow.operators.bash import BashOperator
 from datetime import datetime
+import requests
+import json
 
+
+url_variables = "https://raw.githubusercontent.com/arturfc/docker-airflow-ETL-pipeline/main/datasets/variables_results.csv"
+url_results = "https://raw.githubusercontent.com/arturfc/docker-airflow-ETL-pipeline/main/datasets/backtest_results.csv"
+
+#df_variables = pd.read_csv(url_variables, index_col=[0])
+#df_results = pd.read_csv(url_results, index_col=[0])
+
+#df_results.info()
+#url = 'https://data.cityofnewyork.us/resource/rc75-m7u3.json'
+#response = requests.get(url)
+#test = pd.DataFrame(json.loads(response.content))
+#type(df_results.to_json())
+
+#make it seriable into json format
+#df = df_results.to_json()
+
+#converting back again
+#df = pd.read_json(df)
 
 #ingesting variables_input dataset
 def variables_inputs():
-    df_variables = pd.read_csv("variables_results.csv", index_col=[0])
+    df_variables = pd.read_csv(url_variables, index_col=[0])
+    df_variables = df_variables.to_json()
     return df_variables
 
 #ingesting results dataset
 def results_raw():
-    df_results = pd.read_csv("backtest_results.csv", index_col=[0])
-    df_results.index.name = 'index'
+    df_results = pd.read_csv(url_results, index_col=[0])
+    df_results = df_results.to_json()
     return df_results
 
 
@@ -21,9 +43,16 @@ def results_clean(ti): #ti
     df_variables = ti.xcom_pull(task_ids = 'variables_inputs')
     df_results = ti.xcom_pull(task_ids = 'results_raw')
 
+    df_variables = pd.read_json(df_variables)
+    df_results = pd.read_json(df_results)
+
+    df_results.index.name = 'index'
+
     df_results_clean = pd.merge(df_results[['Saldo Líquido','Taxa de acerto']],
          df_variables[['results_index','RSI_enable','RSI_period','RSI_value']],
          how = "right", left_on=['index'], right_on=['results_index']).drop('results_index', 1)
+
+    df_results_clean = df_results_clean.to_json()
 
     return df_results_clean
 
@@ -31,9 +60,13 @@ def results_clean(ti): #ti
 def best_RSI_results(ti):
     df_results_clean = ti.xcom_pull(task_ids = 'results_clean')
 
+    df_results_clean = pd.read_json(df_results_clean)
+
     df_best_RSI_results = df_results_clean.loc[(df_results_clean['RSI_enable'] == 1) &
                         (df_results_clean['Saldo Líquido'] > 500) &
                         (df_results_clean['Taxa de acerto'] > 0.60)].sort_values(by=['Saldo Líquido'], ascending=False)
+
+    df_best_RSI_results = df_best_RSI_results.to_json()
 
     return df_best_RSI_results
 
